1c4f54c083ad178f5386fd6992479c08d504c386
RamonSynoro <ramon.fossa@triibo.com.br>
2023-07-19 18:30:24 -0300

    feature: Ball changing by button click

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,8 +11,38 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    var lastIndex: Int = 0
 
-
-
+    @IBOutlet weak var imageBallOutlet: UIImageView!
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.becomeFirstResponder()
+        imageBallOutlet.image = ballArray[0]
+    }
+    
+    func sortNewNumber() -> Int {
+        print("sortNewNumber() -> Entrou")
+        let newNum: Int = Int.random(in: 0..<self.ballArray.count)
+        
+        
+        print("sortNewNumber() -> NewNumber: \(newNum)")
+        
+        if newNum != self.lastIndex {
+            self.lastIndex = newNum
+            return newNum
+        }
+        
+        
+        print("sortNewNumber() -> Mesmo número")
+        return sortNewNumber()
+    }
+    
+    @IBAction func buttonClickAction(_ sender: Any) {
+        let newIndex = sortNewNumber()
+        
+        imageBallOutlet.image = ballArray[newIndex]
+    }
 }
 
